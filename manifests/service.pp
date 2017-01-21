f2949f423dbca6af3bbbd20b97cd90da8564aa23
Alban Peignier <alban@tryphon.eu>
2017-01-21 09:40:58 +0100

    Enable service to restart it

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,7 +2,8 @@ class logstash::service {
   service { 'logstash':
     ensure  => running,
     require => Package['logstash'],
-    restart => 'invoke-rc.d logstash reload'
+    restart => 'invoke-rc.d logstash reload',
+    enable => true
   }
 
   File<| tag == 'logstash_config' |> ~> Service['logstash']
